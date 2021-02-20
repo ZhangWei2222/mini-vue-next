7d80bec034d703ef237b4df0b905a9356f3c086c
zhangwei66 <zhangwei66@xiaomi.com>
2021-02-20 15:09:51 +0800

    fix: mini-vue

--- src/reactivity/effect.js
+++ src/reactivity/effect.js
@@ -1,118 +1,33 @@
-import { TriggerOpTypes } from "./operation.js";
-
-export function effect(fn, options = { lazy: false }) {
-  // console.log('effect',fn);
-  
-  // 创建响应式 effect
-  const reactiveEffect = createReactiveEffect(fn, options);
-
-  if (!options.lazy) {
-    // 默认执行
-    reactiveEffect();
-  }
-
-  return reactiveEffect;
-}
-
-//
-let uid = 0;
-let activeEffect;
-const effectStack = [];
-
-function createReactiveEffect(fn, options) {
-  // 创建 响应式 effect
-  const reactiveEffect = function () {
-    // 防止不停更改属性导致死循环
-    if (!effectStack.includes(reactiveEffect)) {
-      try {
-        effectStack.push(reactiveEffect);
-        activeEffect = reactiveEffect; // 将当前 effect 存储到 activeEffect
-        return fn();
-      } finally {
-        // 执行完清空
-        effectStack.pop();
-        activeEffect = effectStack[effectStack.length - 1];
-      }
-    }
-  };
-
-  reactiveEffect.options = options; // effect 的配置信息
-  reactiveEffect.id = uid++; // effect 的id
-  reactiveEffect.deps = []; // 依赖，触发更新的属性
-
-  return reactiveEffect;
-}
-
 const targetMap = new WeakMap();
+let activeEffect = null
+export function effect(eff) {
+  activeEffect = eff
+  activeEffect()
+  activeEffect = null
+}
 
-export function track(target, type, key) {
-  // console.log('target',target,activeEffect);
-  
-  if (activeEffect === undefined) {
-    return; // 说明取值的属性，不依赖于 effect
-  }
-  /*
-   *      key  		       val(map)
-   * {name : 'chris}   {  name : Set(effect,effect) ， age : Set() }
-   * */
-  // 取值
-  let depsMap = targetMap.get(target);
-  // 不存在构建
-  if (!depsMap) {
-    targetMap.set(target, (depsMap = new Map()));
-  }
+export function track(target, key) {
+  if (activeEffect) {
+    let depsMap = targetMap.get(target)
+    if (!depsMap) {
+      targetMap.set(target, depsMap = new Map())
+    }
 
-  let dep = depsMap.get(key);
-  if (!dep) {
-    depsMap.set(key, (dep = new Set()));
-  }
-
-  if (!dep.has(activeEffect)) {
-    dep.add(activeEffect);
-    activeEffect.deps.push(dep); // 当前 effect 记录 dep
+    let dep = depsMap.get(key)
+    if (!dep) {
+      depsMap.set(key, dep = new Set());
+    }
+    dep.add(activeEffect)
   }
 }
 
-export function trigger(target, type, key, value, oldVal) {
-  const depsMap = targetMap.get(target);
-  
-  // 没有依赖收集，直接返回
-  if (!depsMap) return;
-
-  // 计算属性优先于 effect
-  const effects = new Set();
-  const computedRunners = new Set();
-  const add = (effectsToAdd) => {
-    if (effectsToAdd) {
-      effectsToAdd.forEach((effect) => {
-        if (effect.options.computed) {
-          computedRunners.add(effect);
-        } else {
-          effects.add(effect);
-        }
-      });
+export function trigger(target, key) {
+  let depsMap = targetMap.get(target)
+  if (depsMap) {
+    let dep = depsMap.get(key)
+    console.log(dep)
+    if (dep) {
+      dep.forEach(effect => effect())
     }
-  };
-
-  const run = (effect) => {
-    if (effect.options.scheduler) {
-      effect.options.scheduler();
-    } else {
-      effect();
-    }
-  };
-
-  if (key !== null) {
-    add(depsMap.get(key));
   }
-
-  if (type === TriggerOpTypes.ADD) {
-    // 对数组新增熟悉会触发 length 对应的依赖
-    let effects = depsMap.get(Array.isArray(target) ? "length" : "");
-    add(effects);
-  }
-
-  computedRunners.forEach(run);
-  console.log('effects',effects)
-  effects.forEach(run);
 }
